05035b6c4857302a9c5a9542584f6dbb67eb02a6
Seng Wee <sengwee_new@hotmail.com>
2019-03-05 17:42:03 +0800

    updated error dialog logic

--- assignment 2/main.js
+++ assignment 2/main.js
@@ -8,16 +8,52 @@ var applesInput = document.getElementById('apples-input');
 var orangesInput = document.getElementById('oranges-input');
 var bananasInput = document.getElementById('bananas-input');
 
-1
+var appleCost = 0.69;
+var orangeCost = 0.59;
+var bananaCost = 0.39;
 
-$(document).ready(function() {
+var applesError = document.getElementById('apples-error');
+var orangesError = document.getElementById('oranges-error');
+var bananasError = document.getElementById('bananas-error');
+
+var totalCost = document.getElementById('total-cost');
+
+var total = 0;
+
+$(document).ready(function () {
     $('#apples-error').hide();
     $('#oranges-error').hide();
     $('#bananas-error').hide();
 
-    applesInput.addEventListener("click", validateFruitsInput);
-});
+    // $('total-cost').contentEditable = "false";
+    totalCost.contentEditable = "false";
 
-// function validateFruitsInput() {
-//     if
-// }
+    applesInput.addEventListener("input", function (evt) {
+        if (isNaN(this.value)) {
+            $('#apples-error').show();
+        } else {
+            total = total + (this.value) * appleCost;
+            $('#apples-error').hide();
+        }
+    });
+    orangesInput.addEventListener("input", function (evt) {
+        if (isNaN(this.value)) {
+            $('#oranges-error').show();
+        } else {
+            total = total + (this.value) * orangeCost;
+            $('#oranges-error').hide();
+        }
+    });
+    bananasInput.addEventListener("input", function (evt) {
+        if (isNaN(this.value)) {
+            $('#bananas-error').show();
+        } else {
+            total = total + (this.value) * bananaCost;
+            $('#bananas-error').hide();
+        }
+    });
+
+    totalCost.addEventListener("focus", function (evt) {
+        document.getElementById("total-cost").blur();
+    });
+});
