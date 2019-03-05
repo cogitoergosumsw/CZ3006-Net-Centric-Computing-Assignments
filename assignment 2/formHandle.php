79b5cb1830256b84f5396886dd95d085c0d05980
Seng Wee <sengwee_new@hotmail.com>
2019-03-05 21:31:46 +0800

    beautify receipt page

--- assignment 2/formHandle.php
+++ assignment 2/formHandle.php
@@ -10,26 +10,9 @@
 
 <body>
 <?php
-//    initialize the number of apples, oranges and bananas
-$numApples = 0;
-$numOranges = 0;
-$numBananas = 0;
-$totalCost = 0;
-
-//    declaring the output file name
-$fileName = "order.txt";
-
-//    check if the form submitted at the frontend is done through POST method
-if ($SERVER["REQUEST_METHOD"] == "POST") {
-
-//    fetch the data from the submitted form
-    $userName = $_POST['user'];
-    $numApples = $_POST['apples-input'];
-    $numOranges = $_POST['oranges-input'];
-    $numBananas = $_POST['bananas-input'];
-    $totalCost = $_POST['total-cost'];
-
-}
+    // open file on server
+    // throw exception when there's an error opening the file
+    $file = fopen("order.txt", "c+") or die ("Error opening file!")
 ?>
 <div class="ui hidden divider"></div>
 <div class="ui container">
@@ -37,26 +20,54 @@ if ($SERVER["REQUEST_METHOD"] == "POST") {
     <div class="ui clearing divider"></div>
     <div class="row">
         <h3>Thank you for your submission.</h3>
-        <p>This is your Receipt.</p>
     </div>
-    <div class="ui raised segment">
-    <table class="ui celled table">
-        <thead>
-        <tr>
-            <th>Number of Apples</th>
-            <th>Number of Oranges</th>
-            <th>Number of Bananas</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-            <td data-label="apples"><?php echo $_POST['apples-input']; ?></td>
-            <td data-label="oranges"><?php echo $_POST['oranges-input']; ?></td>
-            <td data-label="bananas"><?php echo $_POST['bananas-input']; ?></td>
-        </tr>
-        </tbody>
-    </table>
-    <h3 class="ui">Your preferred mode of payment is <u><?php echo $_POST['payment']?></u>.</h3>
+    <div class="ui hidden divider"></div>
+    <div class="ui placeholder raised segment">
+        <h2 class="ui center aligned header">Receipt</h2>
+        <a class="ui green right corner label">
+            <i class="check icon"></i>
+        </a>
+        <p><strong>Customer's Name</strong>: <?php echo $_POST['user']; ?></p>
+        <h4>Summary</h4>
+        <table class="ui celled table">
+            <thead>
+            <tr>
+                <th>Number of Apples</th>
+                <th>Number of Oranges</th>
+                <th>Number of Bananas</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td data-label="apples"><?php echo $_POST['apples-input']; ?></td>
+                <td data-label="oranges"><?php echo $_POST['oranges-input']; ?></td>
+                <td data-label="bananas"><?php echo $_POST['bananas-input']; ?></td>
+            </tr>
+            </tbody>
+        </table>
+        <div class="ui statistic">
+            <div class="label">
+                Total Cost
+            </div>
+            <div class="value">
+                $<?php
+                $total_cost = 0;
+
+                //listing the cost of each fruit
+                $apple_cost = 0.69;
+                $orange_cost = 0.59;
+                $banana_cost = 0.39;
+
+                //computing total cost from number of fruits
+                $total_cost =
+                    $apple_cost * $_POST['apples-input'] +
+                    $orange_cost * $_POST['oranges-input'] +
+                    $banana_cost * $_POST['bananas-input'];
+                echo ($total_cost);
+                ?>
+            </div>
+        </div>
+        <h3 class="ui">Your preferred mode of payment is <u><?php echo $_POST['payment'] ?></u>.</h3>
     </div>
 </div>
 
