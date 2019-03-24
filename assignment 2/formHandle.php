fe3493e2c517a00b266c4fd0e876a2400ebc81c2
Seng Wee <sengwee_new@hotmail.com>
2019-03-24 21:48:20 +0800

    made the jquery and fontawesome packages available offline

--- assignment 2/formHandle.php
+++ assignment 2/formHandle.php
@@ -3,8 +3,7 @@
     <title>Your Receipt</title>
     <link rel="stylesheet" type="text/css" href="styles/semantic-css/semantic.min.css">
     <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
-          integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles/fontawesome-free-5.8.1-web/css/all.css">
     <link rel="stylesheet" href="styles/style.css">
 </head>
 
