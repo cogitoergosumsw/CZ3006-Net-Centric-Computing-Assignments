a95f760f56bfe792765b87c50fe5f1b6e763d1c6
Seng Wee <sengwee_new@hotmail.com>
2019-03-25 12:13:22 +0800

    refactor main.js

--- assignment 2/main.js
+++ assignment 2/main.js
@@ -30,15 +30,15 @@ $(document).ready(function () {
     // to check for any input from user
     applesInput.addEventListener("input", function (evt) {
         if (validInput(this.value)) {
-            $('#apples-error').show();
-            totalCost.value = "NaN";
-        } else {
             total = ((this.value) * appleCost) + (orangesInput.value * orangeCost) + (bananasInput.value * bananaCost);
             total = parseFloat(total).toFixed(2);
             if (!isNaN(orangesInput.value) && !isNaN(bananasInput.value)) {
                 totalCost.value = total;
             }
             $('#apples-error').hide();
+        } else {
+            $('#apples-error').show();
+            totalCost.value = "NaN";
         }
     });
 
@@ -46,16 +46,16 @@ $(document).ready(function () {
     // to check for any input from user
     orangesInput.addEventListener("input", function (evt) {
         if (validInput(this.value)) {
-            totalCost.value = "NaN";
-            $('#oranges-error').show();
-        } else {
             total = ((this.value) * orangeCost) + (applesInput.value * appleCost) + (bananasInput.value * bananaCost);
             total = parseFloat(total).toFixed(2);
             if (!isNaN(applesInput.value) && !isNaN(bananasInput.value)) {
-				console.log("check oranges");
+                console.log("check oranges");
                 totalCost.value = total;
             }
             $('#oranges-error').hide();
+        } else {
+            totalCost.value = "NaN";
+            $('#oranges-error').show();
         }
     });
 
@@ -63,9 +63,6 @@ $(document).ready(function () {
     // to check for any input from user
     bananasInput.addEventListener("input", function (evt) {
         if (validInput(this.value)) {
-            totalCost.value = "NaN";
-            $('#bananas-error').show();
-        } else {
             total = ((this.value) * bananaCost) + (orangesInput.value * orangeCost) + (applesInput.value * appleCost);
             total = parseFloat(total).toFixed(2);
             if (!isNaN(orangesInput.value) && !isNaN(applesInput.value)) {
@@ -73,13 +70,16 @@ $(document).ready(function () {
                 console.log("total:" + total);
             }
             $('#bananas-error').hide();
+        } else {
+            totalCost.value = "NaN";
+            $('#bananas-error').show();
+
         }
     });
 
     // add an event listener to the Total Cost text input field
     // blur the Total Cost text input field whenever it got focused
     totalCost.addEventListener("focus", function (evt) {
-        console.log("total cost got focused, gonna blur!");
         totalCost.blur();
     });
 
@@ -89,7 +89,7 @@ $(document).ready(function () {
 const form = document.querySelector('form');
 form.addEventListener('submit', event => {
     // check if the form has invalid input from the value of Total Cost element
-    if (totalCost.value == "NaN" || totalCost.value == 0 || validInput(applesInput.value) || validInput(orangesInput.value) || validInput(bananasInput.value)) {
+    if (totalCost.value == "NaN" || totalCost.value == 0 || !validInput(applesInput.value) || !validInput(orangesInput.value) || !validInput(bananasInput.value)) {
         // validate the form submission
         console.log('Form submission cancelled.');
         event.preventDefault();
@@ -107,15 +107,22 @@ function closeModal() {
     ;
 }
 
+// function to check if the given input is an integer
+// to prevent user from submitting floating number values for the quantity of the fruits
 function isAnInteger(x) {
     return x % 1 === 0;
 }
 
+// function to check if the given input is valid
+// criteria for input to be valid
+// 1. any other types of value other than number e.g String or char are not accepted
+// 2. more than 0
+// 3. must be an integer
 function validInput(x) {
-	if (isNaN(x)|| x < 0 || !(isAnInteger(x))) {
-		return true;
-	}
-	return false;
+    if (isNaN(x)|| x < 0 || !(isAnInteger(x))) {
+        return false;
+    }
+    return true;
 }
 
 
